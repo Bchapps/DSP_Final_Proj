04acd66a281745715b678d3e491f3dd7621d38d8
MarloAddie <54915196+MarloAddie@users.noreply.github.com>
2023-04-19 15:15:56 -0600

    Add files via upload

--- Q2_3.m
+++ Q2_3.m
@@ -18,7 +18,7 @@ title('Initial xxbad data')
 
 fs = 8000; % Hz. Sampling rate
 
-sound(xxbad); %Converts matrix of signal data to sound. Matlab function.
+%sound(xxbad); %Converts matrix of signal data to sound. Matlab function.
 %Confirms interference is so strong that the speech is unrecognizable.
 
 figure(2)
@@ -31,27 +31,42 @@ title('Initial Spectrogram')
 % Combine the cascaded filters into one equivalent FIR filter, and give the
 % filter coefficients of the equivalent filter.
 
-%omega = (2*pi*60)/8000;
+omega1 = (1555/8000)*2*pi;
+omega2 = (2222/8000)*2*pi;
+
+b0 = 1; 
+b1 = -2*cos(omega1);
+b2 = 1;
+
+b3 = 1; 
+b4 = -2*cos(omega2);
+b5 = 1;
+
+coeff = [b0, b1, b2];
+coeff2 = [b3, b4, b5];
+
+
+% x = (b0 + b1 + b2);
+% x2 = (b3 + b4 + b5);
 % 
-% b0 = 1; 
-% b1 = -2cos(omega);
-% b2 = 1;
+ x3 = conv(coeff,coeff2);
+
+% b0 = 1;
+% b1 = -2*cos(0.1943*2pi)-2*cos(0.2778*2pi);
+% b2 = 1 + 4*cos(0.1943*2pi)*cos(0.2778*2pi);
+% b3 = -2*cos(0.1943*2pi)-2*cos(0.2778*2pi);
+% b4 = 1;
+
+%coeff = [b0,b1,b2,b3,b4];
 
 
-b0 = 1;
-b1 = -2*cos(0.1943*pi)-2*cos(0.2778*pi);
-b2 = 1 + 4*cos(0.1943*pi)*cos(0.2778*pi);
-b3 = -2*cos(0.1943*pi)-2*cos(0.2778*pi);
-b4 = 1;
-
-coeff = [b0,b1,b2,b3,b4];
-
-
-filt = conv(xxbad, coeff);
+%filt = conv(xxbad, coeff);
+filt = conv(xxbad, x3);
 
 w = -pi:(pi/100):pi;
 figure(3);
-H = freqz(coeff, 1, w);
+%H = freqz(x3, 1, w);
+H = freqz(filt, 1, w);
 plot(w, abs(H));
 title('Frequency Response of Cascaded Filter');
 
