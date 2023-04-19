d9497558d26874a219f0091ea9f8e09809b96c68
MarloAddie <54915196+MarloAddie@users.noreply.github.com>
2023-04-19 14:10:35 -0600

    Add files via upload

--- Q2_3.m
+++ Q2_3.m
@@ -1,7 +1,7 @@
 % Lab S-5 Problem 2.3 - Group 4
 % Removing Interference from a Speech Signal
 
-%Part a: Load speechbad (xxbad), listen (???) to signal, make a spectrogram
+%Part a: Load speechbad (xxbad), listen to signal, make a spectrogram
 %in dB. 
 
 clc
@@ -22,12 +22,40 @@ sound(xxbad); %Converts matrix of signal data to sound. Matlab function.
 %Confirms interference is so strong that the speech is unrecognizable.
 
 figure(2)
-spectrogram(xxbad) %Not a matlab toolbox. Illegal?
+spectrogram(xxbad)
+title('Initial Spectrogram')
 
-% Making a spectrogram: Short-Time Fourier Transform
-freq = 60; %Hz, fundamental frequency
-t = 3; %seconds
-timeaxis = 0:(1/fs):t;
+% Part b: Design a cascade of two FIR Nulling filters to remove the
+% sinusoids completely. This can be accomplished by finding the numerical 
+% values of the filter coefficients for each second-order nulling filter.
+% Combine the cascaded filters into one equivalent FIR filter, and give the
+% filter coefficients of the equivalent filter.
+
+%omega = (2*pi*60)/8000;
+% 
+% b0 = 1; 
+% b1 = -2cos(omega);
+% b2 = 1;
 
 
-%[s,f,t] = stft(xxbad, fs);
+b0 = 1;
+b1 = -2*cos(0.1943*pi)-2*cos(0.2778*pi);
+b2 = 1 + 4*cos(0.1943*pi)*cos(0.2778*pi);
+b3 = -2*cos(0.1943*pi)-2*cos(0.2778*pi);
+b4 = 1;
+
+coeff = [b0,b1,b2,b3,b4];
+
+
+filt = conv(xxbad, coeff);
+
+w = -pi:(pi/100):pi;
+figure(3);
+H = freqz(coeff, 1, w);
+plot(w, abs(H));
+title('Frequency Response of Cascaded Filter');
+
+figure(4);
+spectrogram(filt)
+
+sound(filt)
